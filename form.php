d47263445c576b47921acebdaa705465a009a94d
camiloquiroga93 <camilosolquiroga@gmail.com>
2021-10-14 20:59:36 -0300

    tercer commit

--- form.php
+++ form.php
@@ -1,31 +1,33 @@
-<?php 
- 
-$connection = new mysqli ("localhost","root","","tpalt");
+<?php
 
-$name = '';
-$telefono = '';
-$correo = '';
-$mensaje = '';
+$connection = new mysqli("localhost", "root", "", "tpalt");
 
-if (! empty ($_POST['nombre']) && is_string ($_POST['nombre']) && ! empty($_POST['telefono']) && is_string ($_POST['telefono'])
-&& ! empty ($_POST['correo']) && is_string ($_POST['correo']) && ! empty($_POST['mensaje']) && is_string($_POST['mensaje'])) {
+$name =  trim( $_POST['name'] );
+$telefono = trim( $_POST['telefono'] );;
+$correo = trim( $_POST['correo'] );;
+$mensaje = trim( $_POST['mensaje'] );;
 
-$nombre = mysqli_real_escape_string($connection, $_POST['nombre']); 
-$telefono = mysqli_real_escape_string($connection, $_POST['telefono']);
-$correo = mysqli_real_escape_string($connection, $_POST['correo']);
-$mensaje =mysqli_real_escape_string( $connection, $_POST['mensaje']);
+if (
+    !empty( $_POST["nombre"]) && is_string($_POST["nombre"]) &&
+    !empty( $_POST["telefono"]) && is_string($_POST["telefono"]) &&
+    !empty( $_POST["correo"]) && is_string($_POST["correo"]) &&
+    !empty( $_POST["mensaje"]) && is_string($_POST["mensaje"])
 
-}
-  else {
-    header('Location: index.html');
-  }
+) {
+    $nombre = $connection->real_escape_string($connection, $_POST["nombre"]);
+    $telefono = $connection->real_escape_string($connection, $_POST["telefono"]);
+    $correo = $connection->real_escape_string($connection, $_POST["correo"]);
+    $mensaje = $connection->real_escape_string($connection, $_POST["mensaje"]);
 
-$result = $connection->query ("INSERT INTO formulario (Nombre, Telefono, Correo, Mensaje) VALUES ('$nombre','$telefono','$correo','$mensaje')");
-
-
-if($result){
-  header('Location: index.html');
+} else {
+    header("Location: index.html");
 }
 
- 
-?>
+$result = $connection->query(
+    "INSERT INTO formulario (Nombre, Telefono, Correo, Mensaje) VALUES ('$nombre','$telefono','$correo','$mensaje')"
+);
+
+if ( $result ) = {
+    header("Location: index.html");
+}
+}
