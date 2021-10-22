5ae94a3ee806cf54bf1e5c03b7b099b97a9db58a
camiloquiroga93 <camilosolquiroga@gmail.com>
2021-10-22 20:13:48 -0300

    arreglos

--- form.php
+++ form.php
@@ -6,9 +6,13 @@ $telefono = trim ($_POST['telefono']);
 $correo = trim ( $_POST['correo'] );
 $mensaje = trim ($_POST['mensaje']);
 
-if (!empty( $_POST["nombre"]) && is_string($_POST["nombre"]) &&    !empty( $_POST["telefono"]) && is_string($_POST["telefono"]) &&
-    !empty( $_POST["correo"]) && is_string($_POST["correo"]) &&    !empty( $_POST["mensaje"]) && is_string($_POST["mensaje"]))
-    else { header("Location: index.html"); } 
+if (!empty( $_POST["nombre"]) && is_string($_POST["nombre"]) &&    
+    !empty( $_POST["telefono"]) && is_string($_POST["telefono"]) &&
+    !empty( $_POST["correo"]) && is_string($_POST["correo"]) &&    
+    !empty( $_POST["mensaje"]) && is_string($_POST["mensaje"]);
+    {
+else  header("Location: index.html"); 
+        } 
 
 $result = $connection->query( "INSERT INTO formulario (Nombre, Telefono, Correo, Mensaje) VALUES ('$nombre','$telefono','$correo','$mensaje')");
   if ( $result )  { 
@@ -20,7 +24,6 @@ $result = $connection->query( "INSERT INTO formulario (Nombre, Telefono, Correo,
      else { ?> <h3 class="bad">¡Por favor completa los campos!</h3>
     <?php  }
 
-//?>
 //header("Location: index.html"); 
  //{ $nombre = $connection->real_escape_string($connection, $_POST["nombre"]);
    // $telefono = $connection->real_escape_string($connection, $_POST["telefono"]);
