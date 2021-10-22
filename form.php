da98c0a5b2b5d63e1ea47519e40c962ac956bf2e
camiloquiroga93 <camilosolquiroga@gmail.com>
2021-10-22 20:10:44 -0300

    arreglos

--- form.php
+++ form.php
@@ -1,24 +1,17 @@
 <?php
 
 $connection = new mysqli("localhost", "root", "", "tpalt");
-
 $name =  trim ($_POST['nombre']);
-$telefono = trim ($_POST['telefono']);;
-$correo = trim ( $_POST['correo'] );;
-$mensaje = trim ($_POST['mensaje']);;
+$telefono = trim ($_POST['telefono']);
+$correo = trim ( $_POST['correo'] );
+$mensaje = trim ($_POST['mensaje']);
 
 if (!empty( $_POST["nombre"]) && is_string($_POST["nombre"]) &&    !empty( $_POST["telefono"]) && is_string($_POST["telefono"]) &&
-    !empty( $_POST["correo"]) && is_string($_POST["correo"]) &&    !empty( $_POST["mensaje"]) && is_string($_POST["mensaje"])) 
-  //{ $nombre = $connection->real_escape_string($connection, $_POST["nombre"]);
-   // $telefono = $connection->real_escape_string($connection, $_POST["telefono"]);
-  //  $correo = $connection->real_escape_string($connection, $_POST["correo"]);
-  //  $mensaje = $connection->real_escape_string($connection, $_POST["mensaje"]);
-//}
- else { header("Location: index.html");
-}
+    !empty( $_POST["correo"]) && is_string($_POST["correo"]) &&    !empty( $_POST["mensaje"]) && is_string($_POST["mensaje"]))
+    else { header("Location: index.html"); } 
 
 $result = $connection->query( "INSERT INTO formulario (Nombre, Telefono, Correo, Mensaje) VALUES ('$nombre','$telefono','$correo','$mensaje')");
-if ( $result )  { 
+  if ( $result )  { 
     ?>    <h3 class="ok">¡Mensaje enviado! </h3>
     <?php } 
     else {
@@ -26,5 +19,11 @@ if ( $result )  {
     <?php}
      else { ?> <h3 class="bad">¡Por favor completa los campos!</h3>
     <?php  }
-?>
-//header("Location: index.html");
+
+//?>
+//header("Location: index.html"); 
+ //{ $nombre = $connection->real_escape_string($connection, $_POST["nombre"]);
+   // $telefono = $connection->real_escape_string($connection, $_POST["telefono"]);
+  //  $correo = $connection->real_escape_string($connection, $_POST["correo"]);
+  //  $mensaje = $connection->real_escape_string($connection, $_POST["mensaje"]);
+//}
